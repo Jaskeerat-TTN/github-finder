f4797d80d1853765c86096850aedfcc56b53ef2a
Jaskeerat-TTN <jaskeerat.tungal@tothenew.com>
2022-03-13 22:39:55 +0530

    Final commit

---
+++ src/components/layout/Spinner.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Spinner() {
+    return (
+        <div className="w-100">
+            <div className="spinner mx-auto"></div>
+        </div>
+    );
+}
+
+export default Spinner;
---
+++ src/context/github/GithubContext.js
@@ -0,0 +1,32 @@
+import { createContext, useReducer } from "react";
+import githubReducer from "./GithubReducer";
+
+const GithubContext = createContext();
+
+export const GithubProvider = ({ children }) => {
+    const initialState = {
+        users: [],
+        user: {},
+        repos: [],
+        loading: false,
+    };
+
+    const [state, dispatch] = useReducer(githubReducer, initialState);
+
+    return (
+        <GithubContext.Provider
+            value={{
+                // users: state.users,
+                // user: state.user,
+                // repos: state.repos,
+                // loading: state.loading,
+                ...state,
+                dispatch,
+            }}
+        >
+            {children}
+        </GithubContext.Provider>
+    );
+};
+
+export default GithubContext;
